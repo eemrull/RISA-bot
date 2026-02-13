b061578ccd2bef73c89400c28f243d0128cefd9d
eemrull <emrull5454@gmail.com>
2026-02-14 01:18:55 +0800

    Fix setup.py and remove duplicate servo_controller directory

--- src/control_servo/setup.py
+++ src/control_servo/setup.py
@@ -24,7 +24,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'servo_controller = servo_controller.servo_controller:main',
+            'servo_controller = control_servo.servo_controller:main',
         ],
     },
  )
